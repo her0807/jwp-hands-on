a2f7cb3dda29c241fa3797b1ce10113e46a6196c
her0807 <her0807@naver.com>
2022-10-11 01:30:22 +0900

    test: 커넥션 풀 학습 완료

--- connectionpool/src/test/java/jdbc/stage2/Stage2Test.java
+++ connectionpool/src/test/java/jdbc/stage2/Stage2Test.java
@@ -35,6 +35,7 @@ class Stage2Test {
         final var hikariPool = getPool((HikariDataSource) dataSource);
 
         // 설정한 커넥션 풀 최대값보다 더 많은 스레드를 생성해서 동시에 디비에 접근을 시도하면 어떻게 될까?
+        // 다른 쓰레드가 커넥션을 반납할때까지  기다리고 있어야하지 않을까 ?
         final var threads = new Thread[20];
         for (int i = 0; i < threads.length; i++) {
             threads[i] = new Thread(getConnection());
@@ -49,10 +50,10 @@ class Stage2Test {
         }
 
         // 동시에 많은 요청이 몰려도 최대 풀 사이즈를 유지한다.
-        assertThat(hikariPool.getTotalConnections()).isEqualTo(0);
+        assertThat(hikariPool.getTotalConnections()).isEqualTo(5);
 
         // DataSourceConfig 클래스에서 직접 생성한 커넥션 풀.
-        assertThat(hikariDataSource.getPoolName()).isEqualTo("");
+        assertThat(hikariDataSource.getPoolName()).isEqualTo("sudal");
     }
 
     // 데이터베이스에 연결만 하는 메서드. 커넥션 풀에 몇 개의 연결이 생기는지 확인하는 용도.
--- di/src/test/java/nextstep/study/di/stage3/context/DIContainer.java
+++ di/src/test/java/nextstep/study/di/stage3/context/DIContainer.java
@@ -1,5 +1,8 @@
 package nextstep.study.di.stage3.context;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -7,14 +10,50 @@ import java.util.Set;
  */
 class DIContainer {
 
-    private final Set<Object> beans;
+	private final Set<Object> beans;
 
-    public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
-    }
+	public DIContainer(final Set<Class<?>> classes)  {
+		beans = new HashSet<>();
 
-    @SuppressWarnings("unchecked")
-    public <T> T getBean(final Class<T> aClass) {
-        return null;
-    }
+		for (Class clazz : classes) {
+			try {
+				Constructor<?> constructor = clazz.getDeclaredConstructor();  // private 생성자 가져오기
+				constructor.setAccessible(true); // 객체 생성 허용하기
+				final Object instance = constructor.newInstance();
+				beans.add(instance);
+			} catch (ReflectiveOperationException e) {
+				e.printStackTrace();
+			}
+		}
+
+		for (Object obj : beans) {
+			try {
+				initFieldObject(obj);
+			} catch (ReflectiveOperationException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void initFieldObject(Object obj) throws IllegalAccessException {
+		final Field[] declaredFields = obj.getClass().getDeclaredFields();
+		for (Field field : declaredFields) {
+			field.setAccessible(true);
+			for (Object targets : beans) {
+				if (field.getType().isInstance(targets)) {
+					field.set(obj, targets);
+				}
+			}
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public <T> T getBean(final Class<T> aClass) {
+		for (Object obj : beans) {
+			if (aClass.isInstance(obj)) {
+				return (T)obj;
+			}
+		}
+		return null;
+	}
 }
